2f446a62ff2b960c6797ea7936412e20f2a06e1b
DHIBIN VIKASH K P <83573561+Dhibin-Vikash-Core@users.noreply.github.com>
2022-11-21 20:42:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.write("""
+# Multiplication App
+This app returns the answer for Multiplication of two given numbers
+""")
+#Get Input
+
+st.header('Multiplication Calculator')
+
+def user_input_features():
+    n1 = st.number_input('Enter Number 1')
+    n2 =st.number_input('Enter Number 2')
+    return (n1*n2)
+
+g = user_input_features()
+
+st.subheader('Answer')
+st.write(g)
